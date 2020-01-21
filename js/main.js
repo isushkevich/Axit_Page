b9392ac338780400dc43aba79457356ccaf368db
isushkevich <ivan.sushik@gmail.com>
2020-01-21 23:58:09 +0300

    add slider to testimonials section

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-var burger = document.getElementById("burger");
-var menu = document.getElementById("menu");
-var bg = document.getElementById("bg");
+// var burger = document.getElementById("burger");
+// var menu = document.getElementById("menu");
+// var bg = document.getElementById("bg");
 
 
-burger.addEventListener("click", function() {
-    var isCross = this.classList.contains("cross");
+// burger.addEventListener("click", function() {
+//     var isCross = this.classList.contains("cross");
 
-    if (isCross) {
-        this.classList.remove("cross");
-        menu.classList.remove("visible");
-        bg.classList.remove("visible")
-    } else {
-        this.classList.add("cross");
-        menu.classList.add("visible");
-        bg.classList.add("visible");
-    }
+//     if (isCross) {
+//         this.classList.remove("cross");
+//         menu.classList.remove("visible");
+//         bg.classList.remove("visible")
+//     } else {
+//         this.classList.add("cross");
+//         menu.classList.add("visible");
+//         bg.classList.add("visible");
+//     }
 
-})
+// })
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
