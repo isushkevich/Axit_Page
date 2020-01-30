9434c260455c0419612665214bf8c24df1d6e1b5
isushkevich <ivan.sushik@gmail.com>
2020-01-30 04:07:45 +0300

    add responsiveness

--- js/main.js
+++ js/main.js
@@ -22,31 +22,20 @@ $('.slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    dots: true,
     responsive: [{
             breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
